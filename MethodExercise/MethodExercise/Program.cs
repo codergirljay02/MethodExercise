bc1d73518afe8d5c3f8c4319771c829077c01cd9
jailyn wilkins <jaykw1018@gmail.com>
2020-01-17 11:20:59 -0600

    first commit

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {firstName}");
+
+
+
+            Console.WriteLine("What is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine($"My favorite color is {favoriteColor}.");
+
+            Console.WriteLine("What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine($"My favorites animal is a {favoriteAnimal}.");
+
+            Console.WriteLine("What's the name of your favorite band?");
+            var favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"The name of my favorite band is {favoriteBand}.");
+               
+
+               
+        }
+    }
+}
